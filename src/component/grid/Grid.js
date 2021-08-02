12c2816802420c076c5161cb047b13712135ba81
PavelMalta <shamashov87@mail.ru>
2021-08-02 18:40:09 +0300

    remembered properties grid-template-areas, grid-area, grid-template-rows, grid-template-areas

--- src/component/grid/Grid.js
+++ src/component/grid/Grid.js
@@ -3,13 +3,24 @@ import s from "./Grid.module.css"
 
 export const Grid = () => {
     return (
-        <div className={s.gridContainer}>
+        /*<div className={s.gridContainer}>
             <div className={s.item}>1</div>
             <div className={s.item}>2</div>
             <div className={s.item}>3</div>
             <div className={s.item}>4</div>
             <div className={s.item}>5</div>
             <div className={s.item}>6</div>
+        </div>*/
+        <div className={s.gridContainer}>
+            <header className={s.header}>
+                Шапка
+            </header>
+            <aside className={s.side}>
+                Сайдбар
+            </aside>
+            <main className={s.content}>
+                Контент
+            </main>
         </div>
     )
 }

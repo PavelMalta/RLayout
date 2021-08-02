fdfdf6717c56dc52a3c907d5c9cd03a543253e12
PavelMalta <shamashov87@mail.ru>
2021-08-02 17:56:49 +0300

    added Grid component and trained with blocks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import './App.css';
+import s from "./App.module.css"
 import {Grid} from "./component/grid/Grid";
 
 function App() {
   return (
-    <div className="App">
+    <div className={s.app}>
       <Grid/>
     </div>
   );
--- src/component/grid/Grid.js
+++ src/component/grid/Grid.js
@@ -3,8 +3,13 @@ import s from "./Grid.module.css"
 
 export const Grid = () => {
     return (
-        <div>
-            hello
+        <div className={s.gridContainer}>
+            <div className={s.item}>1</div>
+            <div className={s.item}>2</div>
+            <div className={s.item}>3</div>
+            <div className={s.item}>4</div>
+            <div className={s.item}>5</div>
+            <div className={s.item}>6</div>
         </div>
     )
 }
